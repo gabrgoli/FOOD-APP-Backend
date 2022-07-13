bdca086bbec6819e914513b29d3d0b26491d3056
gabrgoli <ggoliger@yahoo.com>
2022-07-13 08:03:36 -0300

    subir a heroku

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 
 
-let guardarApiEnBDD=false;
+let guardarApiEnBDD=true;
 
 /*const getInstructions = (recipe) =>{
   let texto=''
